551cbfcab4fd563e8ebd3a15b3d8b59337dddda9
joyroybdu <joyroy2002025@gmail.com>
2025-03-15 22:09:24 +0600

    second

--- src/components/UI/Footers.jsx
+++ src/components/UI/Footers.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import footerContact from '../../api/footerApi.json';
-
+import { FaCircleInfo } from "react-icons/fa6";
+import { TbDeviceVisionPro } from "react-icons/tb";
+import { MdMiscellaneousServices } from "react-icons/md";
 export const Footers = () => {
-  return (
-    <footer className='footer-section'>
-      <div className='container grid grid-three-cols'>
-        {footerContact.map((curData, index) => {
-          const { icon, title, details } = curData;
-          return (
-            <div className='footer-contact' key={index}>
-              <div className="icon">{icon}</div>
-              <div className="footer-contact-text">
-                <p>{title}</p>
-                <p>{details}</p>
+    const footerIcon = {
+      info: <FaCircleInfo />,
+      vision: <TbDeviceVisionPro />,
+      services: <MdMiscellaneousServices />
+    };
+
+    return (
+      <footer className='footer-section'>
+        <div className='container grid grid-three-cols'>
+          {footerContact.map((curData, index) => {
+            const { icon, title, details } = curData;
+            return (
+              <div className='footer-contact' key={index}>
+                <div className="icon">{footerIcon[icon]}</div> 
+                <div className="footer-contact-text">
+                  <p>{title}</p>
+                  <p>{details}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
-      </div>
-    </footer>
-  );
+            );
+          })}
+        </div>
+      </footer>
+    );
 };
